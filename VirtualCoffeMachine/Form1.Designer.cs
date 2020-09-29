184896d8d72917d510d424095eb75ff501be88f9
Victor Rodrigues <victorluizrodrigues@gmail.com>
2020-09-28 21:28:59 -0300

    Upando o projeto base.

---
+++ VirtualCoffeMachine/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace VirtualCoffeMachine
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
+            this.LblSaldo = new System.Windows.Forms.Label();
+            this.btn10centavos = new System.Windows.Forms.Button();
+            this.btn25centavos = new System.Windows.Forms.Button();
+            this.btn50centavos = new System.Windows.Forms.Button();
+            this.btn1real = new System.Windows.Forms.Button();
+            this.btnCafe = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btn5centavos = new System.Windows.Forms.Button();
+            this.btn1centavo = new System.Windows.Forms.Button();
+            this.btnCappuccino = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnCafeLeite = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LblSaldo
+            // 
+            this.LblSaldo.AutoSize = true;
+            this.LblSaldo.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblSaldo.Location = new System.Drawing.Point(12, 9);
+            this.LblSaldo.Name = "LblSaldo";
+            this.LblSaldo.Size = new System.Drawing.Size(73, 22);
+            this.LblSaldo.TabIndex = 0;
+            this.LblSaldo.Text = "Saldo: ";
+            // 
+            // btn10centavos
+            // 
+            this.btn10centavos.BackColor = System.Drawing.SystemColors.Window;
+            this.btn10centavos.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn10centavos.Image = ((System.Drawing.Image)(resources.GetObject("btn10centavos.Image")));
+            this.btn10centavos.Location = new System.Drawing.Point(12, 259);
+            this.btn10centavos.Name = "btn10centavos";
+            this.btn10centavos.Size = new System.Drawing.Size(100, 55);
+            this.btn10centavos.TabIndex = 3;
+            this.btn10centavos.Text = "10 Centavos";
+            this.btn10centavos.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn10centavos.UseVisualStyleBackColor = false;
+            // 
+            // btn25centavos
+            // 
+            this.btn25centavos.BackColor = System.Drawing.SystemColors.Window;
+            this.btn25centavos.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn25centavos.Image = ((System.Drawing.Image)(resources.GetObject("btn25centavos.Image")));
+            this.btn25centavos.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn25centavos.Location = new System.Drawing.Point(572, 137);
+            this.btn25centavos.Name = "btn25centavos";
+            this.btn25centavos.Size = new System.Drawing.Size(100, 55);
+            this.btn25centavos.TabIndex = 4;
+            this.btn25centavos.Text = "25 Centavos";
+            this.btn25centavos.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn25centavos.UseVisualStyleBackColor = false;
+            // 
+            // btn50centavos
+            // 
+            this.btn50centavos.BackColor = System.Drawing.SystemColors.Window;
+            this.btn50centavos.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn50centavos.Image = ((System.Drawing.Image)(resources.GetObject("btn50centavos.Image")));
+            this.btn50centavos.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn50centavos.Location = new System.Drawing.Point(572, 198);
+            this.btn50centavos.Name = "btn50centavos";
+            this.btn50centavos.Size = new System.Drawing.Size(100, 55);
+            this.btn50centavos.TabIndex = 5;
+            this.btn50centavos.Text = "50 Centavos";
+            this.btn50centavos.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn50centavos.UseVisualStyleBackColor = false;
+            // 
+            // btn1real
+            // 
+            this.btn1real.BackColor = System.Drawing.SystemColors.Window;
+            this.btn1real.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1real.Image = ((System.Drawing.Image)(resources.GetObject("btn1real.Image")));
+            this.btn1real.Location = new System.Drawing.Point(572, 259);
+            this.btn1real.Name = "btn1real";
+            this.btn1real.Size = new System.Drawing.Size(100, 55);
+            this.btn1real.TabIndex = 6;
+            this.btn1real.Text = "1 Real";
+            this.btn1real.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn1real.UseVisualStyleBackColor = false;
+            // 
+            // btnCafe
+            // 
+            this.btnCafe.BackColor = System.Drawing.SystemColors.Control;
+            this.btnCafe.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCafe.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnCafe.Image = ((System.Drawing.Image)(resources.GetObject("btnCafe.Image")));
+            this.btnCafe.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnCafe.Location = new System.Drawing.Point(187, 70);
+            this.btnCafe.Name = "btnCafe";
+            this.btnCafe.Size = new System.Drawing.Size(150, 150);
+            this.btnCafe.TabIndex = 7;
+            this.btnCafe.Text = "Café: R$2,00";
+            this.btnCafe.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnCafe.UseVisualStyleBackColor = false;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(300, 413);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(100, 40);
+            this.btnCancelar.TabIndex = 11;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btn5centavos
+            // 
+            this.btn5centavos.BackColor = System.Drawing.SystemColors.Window;
+            this.btn5centavos.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5centavos.Image = ((System.Drawing.Image)(resources.GetObject("btn5centavos.Image")));
+            this.btn5centavos.Location = new System.Drawing.Point(12, 198);
+            this.btn5centavos.Name = "btn5centavos";
+            this.btn5centavos.Size = new System.Drawing.Size(100, 55);
+            this.btn5centavos.TabIndex = 12;
+            this.btn5centavos.Text = "5 Centavos";
+            this.btn5centavos.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn5centavos.UseVisualStyleBackColor = false;
+            // 
+            // btn1centavo
+            // 
+            this.btn1centavo.BackColor = System.Drawing.SystemColors.Window;
+            this.btn1centavo.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1centavo.Image = ((System.Drawing.Image)(resources.GetObject("btn1centavo.Image")));
+            this.btn1centavo.Location = new System.Drawing.Point(12, 137);
+            this.btn1centavo.Name = "btn1centavo";
+            this.btn1centavo.Size = new System.Drawing.Size(100, 55);
+            this.btn1centavo.TabIndex = 13;
+            this.btn1centavo.Text = "1 Centavo";
+            this.btn1centavo.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn1centavo.UseVisualStyleBackColor = false;
+            // 
+            // btnCappuccino
+            // 
+            this.btnCappuccino.BackColor = System.Drawing.SystemColors.Control;
+            this.btnCappuccino.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCappuccino.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnCappuccino.Image = ((System.Drawing.Image)(resources.GetObject("btnCappuccino.Image")));
+            this.btnCappuccino.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnCappuccino.Location = new System.Drawing.Point(362, 70);
+            this.btnCappuccino.Name = "btnCappuccino";
+            this.btnCappuccino.Size = new System.Drawing.Size(150, 150);
+            this.btnCappuccino.TabIndex = 14;
+            this.btnCappuccino.Text = "Cappuccino: R$3,50";
+            this.btnCappuccino.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnCappuccino.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.Location = new System.Drawing.Point(187, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 150);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Mocha: R$4,00";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // btnCafeLeite
+            // 
+            this.btnCafeLeite.BackColor = System.Drawing.SystemColors.Control;
+            this.btnCafeLeite.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCafeLeite.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnCafeLeite.Image = ((System.Drawing.Image)(resources.GetObject("btnCafeLeite.Image")));
+            this.btnCafeLeite.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnCafeLeite.Location = new System.Drawing.Point(362, 226);
+            this.btnCafeLeite.Name = "btnCafeLeite";
+            this.btnCafeLeite.Size = new System.Drawing.Size(150, 150);
+            this.btnCafeLeite.TabIndex = 16;
+            this.btnCafeLeite.Text = "Café c/ Leite: R$3,00";
+            this.btnCafeLeite.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnCafeLeite.UseVisualStyleBackColor = false;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 461);
+            this.Controls.Add(this.btnCafeLeite);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnCappuccino);
+            this.Controls.Add(this.btn1centavo);
+            this.Controls.Add(this.btn5centavos);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnCafe);
+            this.Controls.Add(this.btn1real);
+            this.Controls.Add(this.btn50centavos);
+            this.Controls.Add(this.btn25centavos);
+            this.Controls.Add(this.btn10centavos);
+            this.Controls.Add(this.LblSaldo);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Menu";
+            this.Text = "Virtual Coffee";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblSaldo;
+        private System.Windows.Forms.Button btn10centavos;
+        private System.Windows.Forms.Button btn25centavos;
+        private System.Windows.Forms.Button btn50centavos;
+        private System.Windows.Forms.Button btn1real;
+        private System.Windows.Forms.Button btnCafe;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btn5centavos;
+        private System.Windows.Forms.Button btn1centavo;
+        private System.Windows.Forms.Button btnCappuccino;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnCafeLeite;
+    }
+}
+
---
+++ VirtualCoffeMachine/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VirtualCoffeMachine
+{
+    public partial class Menu : Form
+    {
+        //VARIAVEIS GLOBAIS
+        private double SaldoTotal = 0;
+        private enum TiposCafe { Café, Cappuccino, Mocha, CaféLeite }
+        private DialogResult message;
+        public Menu()
+        {
+            //INICIALIZA COMPONENTES DO MENU
+            InitializeComponent();
+
+            LblSaldo.Text = "Saldo: R$" + SaldoTotal;
+
+            //DEFICINICAO DE ACTION PARA BOTOES MOEDA
+            btn1centavo.Click += btnsMoeda_Click;
+            btn5centavos.Click += btnsMoeda_Click;
+            btn10centavos.Click += btnsMoeda_Click;
+            btn25centavos.Click += btnsMoeda_Click;
+            btn50centavos.Click += btnsMoeda_Click;
+            btn1real.Click += btnsMoeda_Click;
+
+            //DEFINICAO DE ACTION PARA BOTOES CAFE
+            btnCafe.Click += btnsCafe_Click;
+            btnCappuccino.Click += btnsCafe_Click;
+            btnMocha.Click += btnsCafe_Click;
+            btnCafeLeite.Click += btnsCafe_Click;
+        }
+
+        //METODO ACTION BOTOES CAFES
+        private void btnsCafe_Click(object sender, EventArgs e)
+        {
+            //CONVERTE O SENDER PARA BUTTON CAFE
+            Button btnsCafe = sender as Button;
+
+            //PERCORE OS TIPOS DE BOTOES DE CAFE
+            switch (btnsCafe.Name)
+            {
+                case "btnCafe": ComprarCafe(TiposCafe.Café); break;
+                case "btnCappuccino": ComprarCafe(TiposCafe.Cappuccino); break;
+                case "btnMocha": ComprarCafe(TiposCafe.Mocha); break;
+                case "btnCafeLeite": ComprarCafe(TiposCafe.CaféLeite); break;
+            }
+        }
+
+        //METODO ACTION BOTOES MOEDA
+        private void btnsMoeda_Click(object sender, EventArgs e)
+        {
+            //CONVERTE O SENDER PARA BUTTON MOEDA
+            Button btnsMoeda = sender as Button;
+
+            //PERCORE OS TIPOS DE BOTOES DE MOEDAS
+            switch (btnsMoeda.Name)
+            {
+                //MOEDA 1 CENTAVO
+                case "btn1centavo": AdicionarSaldo(0.01); break;
+                
+                //MOEDA 5 CENTAVOS
+                case "btn5centavos": AdicionarSaldo(0.05); break;
+
+                //MOEDA 10 CENTAVOS
+                case "btn10centavos": AdicionarSaldo(0.10); break;
+
+                //MOEDA 25 CENTAVOS
+                case "btn25centavos": AdicionarSaldo(0.25); break;
+
+                //MOEDA 50 CENTAVOS
+                case "btn50centavos": AdicionarSaldo(0.50); break;
+
+                //MOEDA 1 REAL
+                case "btn1real": AdicionarSaldo(1); break;
+            }
+            
+        }
+
+        //METODO PARA COMPRAR CAFE NA MAQUINA
+        private void ComprarCafe(TiposCafe tipo)
+        {
+            //PERCORRE OS TIPOS DE CAFE
+            switch (tipo) 
+            {
+                //CAFE
+                case TiposCafe.Café: ChecarSaldo(2); break;
+
+                //CAPPUCCINO
+                case TiposCafe.Cappuccino: ChecarSaldo(3.5); break;
+
+                //MOCHA
+                case TiposCafe.Mocha: ChecarSaldo(4); break;
+
+                //CAFELEITE
+                case TiposCafe.CaféLeite: ChecarSaldo(3); break;
+            }
+        }
+
+        //METODO PARA CHECAR O SALDO DO CLIENTE
+        private void ChecarSaldo(double preco)
+        {
+            //CHECA SE HA SALDO SUFICIENTE
+            if(preco <= SaldoTotal)
+            {
+                //CASO HAJA TROCO
+                if (preco != SaldoTotal)
+                {
+                    message = MessageBox.Show("Compra Confirmada! Retire o Troco: R$" + (SaldoTotal - preco), "Compra Confirmada", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    ZerarSaldo();
+                }
+                //CASO NAO HAJA TROCO
+                else
+                {
+                    message = MessageBox.Show("Compra Confirmada! Retire seu café.", "Compra Confirmada", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    ZerarSaldo();
+                }
+                    
+            }
+            //CASO NAO HAJA SALDO SUFICIENTE
+            else
+            {
+                message = MessageBox.Show("Seu saldo não é suficiente. Favor inserir R$" + Math.Abs(SaldoTotal-preco), "Compra Rejeitada", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        //METODO PARA ADICIONAR SALDO NA MAQUINA
+        private void AdicionarSaldo(double valor)
+        {
+            //CHECA CASO SEJA MOEDA DE 1C OU 5C E REJEITA
+            if(valor.Equals(0.01) || valor.Equals(0.05))
+            {
+                message = MessageBox.Show("Moeda Rejeitada!", "Moeda Inválida", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            //ADICIONA MOEDA AO VALOR TOTAL
+            SaldoTotal += valor;
+            LblSaldo.Text = "Saldo: R$" + SaldoTotal;
+        }
+
+        //METODO PARA ZERAR SALDO
+        private void ZerarSaldo()
+        {
+            SaldoTotal = 0;
+        }
+
+        //METODO PARA O BOTAO CANCELAR
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            message = MessageBox.Show("Retire seu dinheiro!", "Compra Cancelada", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            ZerarSaldo();
+        }
+    }
+}
